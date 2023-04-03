f801e28d65d15eea12360389a85bd9909b76544e
Gabriel <gabriel@gabstep.com.br>
2023-04-03 01:46:16 -0300

    app online / offline - layout fix #fix

--- src/model/App.js
+++ src/model/App.js
@@ -13,25 +13,37 @@ export default class App extends Model {
         this.namespace = namespace
     }
 
+    get replicas() {
+        return {
+            web: this.psScale.web,
+            worker: this.psScale.worker,
+        };
+    }
+
+    get online() {
+        let replicas = 0;
+        for(const key in this.replicas) {
+            replicas += this.replicas[key];
+        }
+        return replicas > 0;
+    }
+
     async refresh({proxyPorts, psScale}) {
         this.proxyPorts = proxyPorts;
-        this.psScale = psScale;
+        this.psScale = {
+            web: psScale.web || 0,
+            worker: psScale.worker || 0,
+        };
     }
 
     getContainers() {
         return []
     }
 
-    get replicas() {
-        return {
-            web: this.psScale.web || 0,
-            worker: this.psScale.worker || 0,
-        };
-    }
-
     toJson() {
         return {
             name: this.name,
+            online: this.online,
             proxyPorts: this.proxyPorts,
             replicas: this.replicas,
         }
