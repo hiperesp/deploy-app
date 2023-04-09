668809414b77b12a067830766d18e7e2301d8337
Gabriel <gabriel@gabstep.com.br>
2023-04-09 01:19:08 -0300

    timeago filter scales fix

--- src/deploy-app.js
+++ src/deploy-app.js
@@ -32,30 +32,37 @@ nunjucksEnv.addFilter('timeago', function(time) {
         return 'just now';
     }
     if(seconds < 60) {
-        return `${seconds} seconds ago`;
+        const scale = seconds === 1 ? 'second' : 'seconds';
+        return `${seconds} ${scale} ago`;
     }
     const minutes = Math.floor(seconds / 60);
     if(minutes < 60) {
-        return `${minutes} minutes ago`;
+        const scale = minutes === 1 ? 'minute' : 'minutes';
+        return `${minutes} ${scale} ago`;
     }
     const hours = Math.floor(minutes / 60);
     if(hours < 24) {
-        return `${hours} hours ago`;
+        const scale = hours === 1 ? 'hour' : 'hours';
+        return `${hours} ${scale} ago`;
     }
     const days = Math.floor(hours / 24);
     if(days < 7) {
-        return `${days} days ago`;
+        const scale = days === 1 ? 'day' : 'days';
+        return `${days} ${scale} ago`;
     }
     const weeks = Math.floor(days / 7);
     if(weeks < 4) {
-        return `${weeks} weeks ago`;
+        const scale = weeks === 1 ? 'week' : 'weeks';
+        return `${weeks} ${scale} ago`;
     }
     const months = Math.floor(days / 30);
     if(months < 12) {
-        return `${months} months ago`;
+        const scale = months === 1 ? 'month' : 'months';
+        return `${months} ${scale} ago`;
     }
     const years = Math.floor(days / 365);
-    return `${years} years ago`;
+    const scale = years === 1 ? 'year' : 'years';
+    return `${years} ${scale} ago`;
 });
 
 // Configurar o cookie parser
